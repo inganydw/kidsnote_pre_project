2c40c3df0a68e8dd9eec58d393fbcc6c7b4600f1
dwyou <dwyou@station3.co.kr>
2023-07-19 23:08:42 +0900

    프로젝트 업로드

---
+++ Kidsnote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kidsnote
+//
+//  Created by YouDongwoo on 2023/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
